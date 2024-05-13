b8b6acb6cca3fa704b48099cf1303c08eac90b03
Luke40172 <luuk.kleiweg@gmail.com>
2024-05-13 21:54:18 +0200

    2023 - Day 6 - Part 2

---
+++ 2023/06/part-2/RaceAnalyzer.php
@@ -0,0 +1,57 @@
+<?php
+
+class RaceAnalyzer
+{
+    private $handle;
+
+    private array $raceList;
+
+    public function __construct(string $file)
+    {
+        $this->loadData($file);
+    }
+
+    private function loadData(string $file): void
+    {
+        $this->handle = fopen($file, 'r');
+        if (!$this->handle) {
+            die('Unable to load puzzle input');
+        }
+
+        $timeList     = $this->parseLine(fgets($this->handle));
+        $distanceList = $this->parseLine(fgets($this->handle));
+
+        $this->raceList = array_map(
+            static fn($time, $distance) => ['t' => $time, 'd' => $distance],
+            $timeList,
+            $distanceList
+        );
+
+        fclose($this->handle);
+    }
+
+    private function parseLine(string $line): array
+    {
+        $line         = trim($line, "\n");
+        $line         = preg_replace("/ +/", '', $line);
+        list(, $list) = explode(':', $line);
+        return [$list];
+    }
+
+
+    public function findPossibleFasterTimes(): \Generator
+    {
+        foreach ($this->raceList as $race) {
+            $time     = $race['t'];
+            $distance = $race['d'];
+            $count    = 0;
+            for ($t = 0; $t <= $time; $t++) {
+                $d = ($t * 1) * ($time - $t);
+                if ($d > $distance) {
+                    $count++;
+                }
+            }
+            yield $count;
+        }
+    }
+}
